4e9f7abb55e51b18d086ca1b8fc7fce98105266d
Tyler Long <tyler4long@gmail.com>
2018-06-11 15:21:46 +0800

    First try of implicit grant

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,12 @@
 </template>
 
 <script>
+import RingCentral from 'ringcentral-js-concise'
+
+import config from './config'
+
+const rc = new RingCentral(config.RINGCENTRAL_CLIENT_ID, '', config.RINGCENTRAL_SERVER_URI)
+
 export default {
   data: function () {
     return {
@@ -14,7 +20,23 @@ export default {
   },
   methods: {
     authorize: function () {
-      this.authorized = true
+      const vm = this
+      const authUri = rc.authorizeUri(config.OAUTH_REDIRECT_URI, { responseType: 'token' })
+      console.log(authUri)
+      var oauthWindow = window.open(authUri, 'oauthWindow', 'width=800, height=600')
+      var oauthInterval = window.setInterval(function () {
+        try {
+          if (oauthWindow.document.URL.indexOf(config.OAUTH_REDIRECT_URI) !== -1) {
+            window.clearInterval(oauthInterval)
+            oauthWindow.close()
+            vm.authorized = true
+            console.log('authorized')
+          }
+        } catch (e) {
+          // cross origin access
+          console.log('not yet authorized')
+        }
+      }, 1000)
     }
   }
 }
