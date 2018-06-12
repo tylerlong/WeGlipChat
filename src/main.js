a6e13d5a57824a1680af51e9009681816161f492
Tyler Long <tyler4long@gmail.com>
2018-06-12 17:32:40 +0800

    element ui css

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Element from 'element-ui'
 
+import 'element-ui/lib/theme-chalk/index.css'
+
 import App from './App'
 import Login from './components/Login'
 import OAuth from './components/OAuth'
