8e93d8c948be57a12826db020e8a743765362eb7
Tyler Long <tyler4long@gmail.com>
2018-06-18 17:52:01 +0800

    Minimize CSS for prod

--- dev-notes.md
+++ dev-notes.md
@@ -1,7 +1,4 @@
-- A separate web server is needed. I use [HTTP Server](https://tylingsoft.com/http-server/).
-
-
 ## todo
 
 - How to make the release package smaller?
-- webpack dev-server
+- tests
--- webpack.prod.babel.js
+++ webpack.prod.babel.js
@@ -1,13 +1,26 @@
 import path from 'path'
 import { mergeDeepRight } from 'ramda'
 import CleanWebpackPlugin from 'clean-webpack-plugin'
+import UglifyJsPlugin from 'uglifyjs-webpack-plugin'
+import OptimizeCSSAssetsPlugin from 'optimize-css-assets-webpack-plugin'
 
 import commonConfig from './webpack.common'
 
 const webpackConfig = mergeDeepRight(commonConfig, {
   mode: 'production',
+  devtool: 'source-map',
   output: {
     path: path.join(__dirname, 'docs')
+  },
+  optimization: {
+    minimizer: [
+      new UglifyJsPlugin({
+        cache: true,
+        parallel: true,
+        sourceMap: true
+      }),
+      new OptimizeCSSAssetsPlugin({})
+    ]
   }
 })
 
