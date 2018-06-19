93777be055c3506a04d362ef2d55026fdadaa324
Tyler Long <tyler4long@gmail.com>
2018-06-19 16:30:27 +0800

    Release

---
+++ docs/precache-manifest.6ea29c152ecefe00ca8b5ade4e25efe5.js
@@ -0,0 +1,20 @@
+self.__precacheManifest = [
+  {
+    "revision": "4522a4a7f5961e957346d00126c0bd66",
+    "url": "oauth.html"
+  },
+  {
+    "revision": "4d050f5324c023334cae2c85e0995ac3",
+    "url": "index.html"
+  },
+  {
+    "url": "index.4b5bf7d0e0cd5a12d4af.js"
+  },
+  {
+    "url": "index.4b5bf7d0e0cd5a12d4af.css"
+  },
+  {
+    "revision": "5726aed1cdfacb2a65197dcaedd07081",
+    "url": "favicon.png"
+  }
+];
