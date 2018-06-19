7f02a3109992eec19234b839a225a0e6c7bb8be9
Tyler Long <tyler4long@gmail.com>
2018-06-19 17:53:32 +0800

    Use f7-page

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <f7-page>
     <p>Welcome<span v-if="extension"> {{ extension.name }}</span>!</p>
     <p><f7-button fill v-if="token" @click="logOut">Log Out</f7-button></p>
     <f7-list media-list v-if="groups">
@@ -10,18 +10,19 @@
         v-for="group in groups"
         :key="group.id"
       >
-        <img slot="media" src="http://via.placeholder.com/72x72" width="42" />
+        <img slot="media" src="https://via.placeholder.com/64x64" width="42" />
       </f7-list-item>
     </f7-list>
-  </div>
+  </f7-page>
 </template>
 
 <script>
 import { mapState } from 'vuex'
-import { f7Button, f7List, f7ListItem } from 'framework7-vue'
+import { f7Page, f7Button, f7List, f7ListItem } from 'framework7-vue'
 
 export default {
   components: {
+    f7Page,
     f7Button,
     f7List,
     f7ListItem
