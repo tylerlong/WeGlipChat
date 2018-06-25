6ccfdc4ff96f0861926181afd06b3d65b13da9d5
Tyler Long <tyler4long@gmail.com>
2018-06-25 17:30:13 +0800

    group name and group image

--- src/components/App.vue
+++ src/components/App.vue
@@ -5,13 +5,13 @@
       <f7-list media-list v-if="groups">
         <f7-list-item
           link="#"
-          :title="group.name || group.members.join(', ')"
+          :title="groupName(group)"
           text="..."
           v-for="group in groups"
           :key="group.id"
           @click="openGroup(group.id)"
         >
-        <img slot="media" :src="groupImage(group)" width="42" />
+        <img slot="media" :src="groupImage(group)" width="42" height="42" />
         </f7-list-item>
       </f7-list>
     </div>
@@ -21,6 +21,7 @@
 <script>
 import { f7Page, f7Tabs, f7Tab, f7Link, f7Toolbar, f7List, f7ListItem } from 'framework7-vue'
 import { mapState } from 'vuex'
+import * as R from 'ramda'
 
 import Settings from './Settings.vue'
 import Tabs from './Tabs.vue'
@@ -38,11 +39,30 @@ export default {
     },
     groupImage (group) {
       if (group.type === 'PrivateChat') {
-        const memberId = group.members.filter(m => m.id !== this.extension.id.toString())[0]
-        return this.persons[memberId].avatar
-      } else {
-        return 'https://via.placeholder.com/64x64'
+        const memberId = group.members.filter(m => m !== this.extension.id.toString())[0]
+        if (this.persons[memberId]) {
+          const avatar = this.persons[memberId].avatar
+          if (R.isNil(avatar)) {
+            return 'https://via.placeholder.com/64x64'
+          }
+          return avatar
+        }
       }
+      return 'https://via.placeholder.com/64x64'
+    },
+    groupName (group) {
+      if (group.type === 'PrivateChat') {
+        const memberId = group.members.filter(m => m !== this.extension.id.toString())[0]
+        const person = this.persons[memberId]
+        if (person) {
+          if (!R.isNil(person.firstName) || !R.isNil(person.lastName)) {
+            return `${person.firstName} ${person.lastName}`
+          } else {
+            return person.email
+          }
+        }
+      }
+      return group.name
     }
   }
 }
