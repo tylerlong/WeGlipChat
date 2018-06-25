e7634562eb0961ec2f42c055422c138118b135a3
Tyler Long <tyler4long@gmail.com>
2018-06-25 11:10:59 +0800

    Display Unsupported message

--- src/components/Group.vue
+++ src/components/Group.vue
@@ -12,6 +12,7 @@
               <a v-else :href="file.contentUri" class="external" target="_blank">{{ file.name }}</a>
             </template>
           </div>
+          <div v-if="post.text === null && post.attachments === null">Unsupported message</div>
         </div>
       </f7-message>
     </f7-messages>
