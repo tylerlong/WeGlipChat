a7f7ac68652f468a4706e95bebee2f8657000174
Tyler Long <tyler4long@gmail.com>
2018-07-09 23:10:40 +0800

    Allow switch theme via query param

--- src/index.js
+++ src/index.js
@@ -12,7 +12,10 @@ import './index.css'
 
 Vue.use(Framework7Vue, Framework7)
 
-const theme = 'auto'
+let theme = 'auto'
+if (document.location.search.indexOf('theme=') >= 0) {
+  theme = document.location.search.split('theme=')[1].split('&')[0]
+}
 
 const app = new Vue({
   router,
