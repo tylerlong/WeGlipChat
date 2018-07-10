f7bebb1b189ec659ffc8cf624993fd6543a614da
Tyler Long <tyler4long@gmail.com>
2018-07-10 13:24:55 +0800

    Update settings page UI

--- src/components/Settings.vue
+++ src/components/Settings.vue
@@ -2,35 +2,42 @@
   <f7-page :page-content="false">
     <tabs active="settings"></tabs>
     <div class="page-content">
-      <div class="settings-content">
-        <p>Welcome<span v-if="extension"> {{ extension.name }}</span>!</p>
-        <p>Enable desktop notification:
-          <label class="toggle">
-            <input type="checkbox" v-model="enableNotifications">
-            <span class="toggle-icon"></span>
-          </label>
-        </p>
-        <p>Theme:
-          <f7-radio
-            name="theme"
-            value="auto"
-            :checked="theme === 'auto' || theme === undefined"
-            @change="theme = $event.target.value"
-          ></f7-radio> Auto |
-          <f7-radio
-            name="theme"
-            value="md"
-            :checked="theme === 'md'"
-            @change="theme = $event.target.value"
-          ></f7-radio> Material Design |
-          <f7-radio
-            name="theme"
-            value="ios"
-            :checked="theme === 'ios'"
-            @change="theme = $event.target.value"
-          ></f7-radio> iOS
-        </p>
+      <h3>Welcome<span v-if="extension"> {{ extension.name }}</span>!</h3>
+      <div class="list simple-list">
+        <ul>
+          <li>
+            <span>Desktop notification</span>
+            <label class="toggle">
+              <input type="checkbox" v-model="enableNotifications">
+              <span class="toggle-icon"></span>
+            </label>
+          </li>
+        </ul>
       </div>
+      <f7-block-title>Theme</f7-block-title>
+      <f7-list>
+        <f7-list-item radio
+          name="theme"
+          value="auto"
+          :checked="theme === 'auto' || theme === undefined"
+          @change="theme = $event.target.value"
+          title="Auto"
+        ></f7-list-item>
+        <f7-list-item radio
+          name="theme"
+          value="md"
+          :checked="theme === 'md'"
+          @change="theme = $event.target.value"
+          title="Material Design"
+        ></f7-list-item>
+        <f7-list-item radio
+          name="theme"
+          value="ios"
+          :checked="theme === 'ios'"
+          @change="theme = $event.target.value"
+          title="iOS"
+        ></f7-list-item>
+      </f7-list>
       <p><f7-button color="green" fill v-if="loggedIn" @click="logOut">Log Out</f7-button></p>
     </div>
   </f7-page>
@@ -38,7 +45,7 @@
 
 <script>
 import { mapState } from 'vuex'
-import { f7Button, f7Page, f7Radio } from 'framework7-vue'
+import { f7Button, f7Page, f7BlockTitle, f7List, f7ListItem } from 'framework7-vue'
 import * as R from 'ramda'
 
 import Tabs from './Tabs.vue'
@@ -46,7 +53,7 @@ import rc from '../api/ringcentral'
 
 export default {
   components: {
-    f7Button, f7Page, f7Radio, Tabs
+    f7Button, f7Page, f7BlockTitle, f7List, f7ListItem, Tabs
   },
   computed: {
     ...mapState(['extension']),
