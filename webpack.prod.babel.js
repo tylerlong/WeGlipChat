e5a74ff2f74a288b981dac1dafe7318956736ec3
Tyler Long <tyler4long@gmail.com>
2018-07-10 15:30:32 +0800

    Disable service worker because I haven't figured out the best practice

--- webpack.prod.babel.js
+++ webpack.prod.babel.js
@@ -3,7 +3,7 @@ import { mergeDeepRight } from 'ramda'
 import UglifyJsPlugin from 'uglifyjs-webpack-plugin'
 import OptimizeCSSAssetsPlugin from 'optimize-css-assets-webpack-plugin'
 import ExtractCssChunksPlugin from 'extract-css-chunks-webpack-plugin'
-import { GenerateSW } from 'workbox-webpack-plugin'
+// import { GenerateSW } from 'workbox-webpack-plugin'
 
 import commonConfig from './webpack.common'
 
@@ -28,7 +28,7 @@ const webpackConfig = mergeDeepRight(commonConfig, {
 
 webpackConfig.module.rules[0].use.unshift(ExtractCssChunksPlugin.loader)
 webpackConfig.plugins.push(new ExtractCssChunksPlugin({ filename: '[name].[chunkhash].css' }))
-webpackConfig.entry.index.splice(1, 0, './src/service-worker.js')
-webpackConfig.plugins.push(new GenerateSW({}))
+// webpackConfig.entry.index.splice(1, 0, './src/service-worker.js')
+// webpackConfig.plugins.push(new GenerateSW({}))
 
 export default webpackConfig
