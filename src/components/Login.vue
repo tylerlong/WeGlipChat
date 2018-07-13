a46603c171b0aed1fd96f525b0225ea0cd7aa493
Tyler Long <tyler4long@gmail.com>
2018-07-13 16:44:30 +0800

    Add download apps link

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -3,6 +3,7 @@
     <div class="text-align-center">
       <h1>WeGlipChat</h1>
       <p>A Glip client inspired by WeChat. <i class="f7-icons">social_github_fill</i> <a href="https://github.com/tylerlong/WeGlipChat" class="external" target="_blank">tylerlong/WeGlipChat</a></p>
+      <h3><i class="f7-icons">cloud_download</i> <a href="https://github.com/tylerlong/wgc-desktop/releases" target="_blank" class="external">Download apps for macOS, Windows & Linux</a> <i class="f7-icons">cloud_download</i></h3>
       <p><f7-button color="green" fill @click="showModal = true">Log In</f7-button></p>
     </div>
     <f7-popup :opened="showModal">
