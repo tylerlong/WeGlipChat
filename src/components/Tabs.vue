e50b804854880b5b5b5431b5809174c7eb1205f2
Tyler Long <tyler4long@gmail.com>
2018-07-27 16:32:30 +0800

    Remove framework7-vue from Tabs.vue

--- src/components/Tabs.vue
+++ src/components/Tabs.vue
@@ -1,18 +1,24 @@
 <template>
-  <f7-toolbar tabbar>
-    <f7-link @click="openTab('root')" tab-link :tab-link-active="active === 'glip'"><f7-icon if-ios="f7:chat" if-md="material:chat"></f7-icon></f7-link>
-    <f7-link @click="openTab('contacts')" tab-link :tab-link-active="active === 'contacts'"><f7-icon if-ios="f7:persons" if-md="material:group"></f7-icon></f7-link>
-    <f7-link @click="openTab('settings')" tab-link :tab-link-active="active === 'settings'"><f7-icon if-ios="f7:settings" if-md="material:settings"></f7-icon></f7-link>
-  </f7-toolbar>
+  <div class="toolbar tabbar">
+    <div class="toolbar-inner">
+      <a @click="openTab('root')" class="tab-link" :class="{ 'tab-link-active': active === 'glip' }">
+        <i class="f7-icons ios-only">chat</i>
+        <i class="material-icons md-only">chat</i>
+      </a>
+      <a @click="openTab('contacts')" class="tab-link" :class="{ 'tab-link-active': active === 'contacts' }">
+        <i class="f7-icons ios-only">persons</i>
+        <i class="material-icons md-only">group</i>
+      </a>
+      <a @click="openTab('settings')" class="tab-link" :class="{ 'tab-link-active': active === 'settings' }">
+        <i class="f7-icons ios-only">settings</i>
+        <i class="material-icons md-only">settings</i>
+      </a>
+    </div>
+  </div>
 </template>
 
 <script>
-import { f7Toolbar, f7Link, f7Icon } from 'framework7-vue'
-
 export default {
-  components: {
-    f7Toolbar, f7Link, f7Icon
-  },
   props: ['active'],
   methods: {
     openTab: function (name) {
