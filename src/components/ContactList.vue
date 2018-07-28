97dab733c046760cfd0f9a6cd3a52c22998a0c32
Tyler Long <tyler4long@gmail.com>
2018-07-28 18:12:56 +0800

    Get rid of framework7-vue from ContactList.vue

--- src/components/ContactList.vue
+++ src/components/ContactList.vue
@@ -2,18 +2,25 @@
   <div class="page">
     <tabs active="contacts"></tabs>
     <div class="page-content">
-      <f7-list media-list v-if="persons && persons.length > 0">
-        <f7-list-item
-          link="#"
-          :title="person.name"
-          :text="person.email"
-          v-for="person in persons"
-          :key="person.id"
-          @click="openPerson(person.id)"
-        >
-        <img slot="media" :src="getPersonAvatar(person.id)" class="avatar-image" />
-        </f7-list-item>
-      </f7-list>
+      <div class="list media-list" v-if="persons && persons.length > 0">
+        <ul>
+          <li v-for="person in persons" :key="person.id" @click="openPerson(person.id)">
+            <div class="item-content">
+              <div class="item-media">
+                <img :src="getPersonAvatar(person.id)" class="avatar-image">
+              </div>
+              <div class="item-inner">
+                <div class="item-title-row">
+                  <div class="item-title">{{ person.name }}</div>
+                  <div class="item-after"></div>
+                </div>
+                <div class="item-subtitle"></div>
+                <div class="item-text">{{ person.email }}</div>
+              </div>
+            </div>
+          </li>
+        </ul>
+      </div>
       <div v-else class="block text-align-center">
         <Preloader></Preloader>
       </div>
@@ -22,7 +29,6 @@
 </template>
 
 <script>
-import { f7List, f7ListItem } from 'framework7-vue'
 import { mapGetters } from 'vuex'
 
 import Tabs from './Tabs.vue'
@@ -30,7 +36,7 @@ import Preloader from './Preloader.vue'
 
 export default {
   components: {
-    f7List, f7ListItem, Tabs, Preloader
+    Tabs, Preloader
   },
   computed: {
     ...mapGetters(['getPersons', 'getPersonAvatar']),
