2cf9c0b9b6b7222a3974a05f90f75bdc7ba44fab
Tyler Long <tyler4long@gmail.com>
2018-07-30 15:55:45 +0800

    Remove framework7-vue

--- src/framework7.js
+++ src/framework7.js
@@ -1,7 +1,8 @@
 import Framework7 from 'framework7'
 import Popup from 'framework7/dist/components/popup/popup.js'
+import Messages from 'framework7/dist/components/messages/messages.js'
 
-Framework7.use([Popup])
+Framework7.use([Popup, Messages])
 const framework7 = new Framework7()
 
 export default framework7
