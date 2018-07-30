b0fbb281df72d3fd8fa1ae25637365b7386c28d6
Tyler Long <tyler4long@gmail.com>
2018-07-30 16:01:57 +0800

    Upgrade framework7

--- src/framework7.js
+++ src/framework7.js
@@ -1,6 +1,8 @@
 import Framework7 from 'framework7'
-import Popup from 'framework7/dist/components/popup/popup.js'
-import Messages from 'framework7/dist/components/messages/messages.js'
+import Popup from 'framework7/components/popup/popup.js'
+import Messages from 'framework7/components/messages/messages.js'
+
+import 'framework7/css/framework7.css'
 
 Framework7.use([Popup, Messages])
 const framework7 = new Framework7()
--- src/index.js
+++ src/index.js
@@ -1,7 +1,4 @@
 import Vue from 'vue'
-import Framework7 from './framework7.esm.bundle'
-import 'framework7/dist/css/framework7.css'
-import Framework7Vue from 'framework7-vue'
 import 'framework7-icons'
 import 'material-design-icons/iconfont/material-icons.css'
 import localforage from 'localforage'
@@ -12,8 +9,6 @@ import store from './store'
 import router from './router'
 
 import './index.css'
-
-Vue.use(Framework7Vue, Framework7)
 
 let theme = 'ios'
 
