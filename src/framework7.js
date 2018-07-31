dfd75898ecb076b04fac4040d1a592f17a76f1a3
Tyler Long <tyler4long@gmail.com>
2018-07-31 17:35:35 +0800

    Do not use window.Framework7

--- src/framework7.js
+++ src/framework7.js
@@ -1,6 +1,9 @@
 import Cookies from 'js-cookie'
+import Framework7 from 'framework7/js/framework7'
+
+import 'framework7/css/framework7.css'
 
 let theme = Cookies.get('FRAMEWORK7_THEME') || 'ios'
-const framework7 = new window.Framework7({ theme })
+const framework7 = new Framework7({ theme })
 
 export default framework7
