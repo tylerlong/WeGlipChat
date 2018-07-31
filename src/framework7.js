25412371548cd905d41f7f0f62e5d174fb984adc
Tyler Long <tyler4long@gmail.com>
2018-07-31 17:25:26 +0800

    Use framework7 from CDN

--- src/framework7.js
+++ src/framework7.js
@@ -1,13 +1,6 @@
-import Framework7 from 'framework7'
-import Popup from 'framework7/components/popup/popup.js'
-import Messages from 'framework7/components/messages/messages.js'
 import Cookies from 'js-cookie'
 
-import 'framework7/css/framework7.css'
-
-Framework7.use([Popup, Messages])
-
 let theme = Cookies.get('FRAMEWORK7_THEME') || 'ios'
-const framework7 = new Framework7({ theme })
+const framework7 = new window.Framework7({ theme })
 
 export default framework7
