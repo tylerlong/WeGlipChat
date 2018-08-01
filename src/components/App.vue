ee8a4a27e6ebec546d047200ec2af524897000fd
Tyler Long <tyler4long@gmail.com>
2018-08-01 15:09:08 +0800

    Search groups

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,8 +1,19 @@
 <template>
   <div class="page">
     <tabs active="glip"></tabs>
+    <form class="searchbar" id="groups-searchbar">
+      <div class="searchbar-inner">
+        <div class="searchbar-input-wrap">
+          <input type="search" placeholder="Search">
+          <i class="searchbar-icon"></i>
+          <span class="input-clear-button"></span>
+        </div>
+        <span class="searchbar-disable-button">Cancel</span>
+      </div>
+    </form>
     <div class="page-content">
-      <div class="list media-list" v-if="groups">
+      <div class="searchbar-backdrop"></div>
+      <div id="groups-list" class="list media-list searchbar-found" v-if="groups">
         <ul>
           <li v-for="group in groups" :key="group.id" @click="openGroup(group.id)">
             <div class="item-content">
@@ -24,15 +35,22 @@
       <div v-else class="block text-align-center">
         <preloader></preloader>
       </div>
+      <div class="list simple-list searchbar-not-found">
+        <ul>
+          <li>No group matched</li>
+        </ul>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 import { mapState, mapGetters } from 'vuex'
+import * as R from 'ramda'
 
 import Tabs from './Tabs.vue'
 import Preloader from './Preloader.vue'
+import framework7 from '../framework7'
 
 export default {
   components: {
@@ -45,6 +63,26 @@ export default {
   methods: {
     openGroup (id) {
       this.$router.push({ name: 'group', params: { id } })
+    }
+  },
+  mounted: function () {
+    this.searchbar = framework7.searchbar.create({
+      el: '#groups-searchbar',
+      searchContainer: '#groups-list',
+      searchIn: '.item-title'
+    })
+  },
+  watch: { // fix refresh page issue
+    groups: function (newVal, oldVal) {
+      if (R.isNil(oldVal) || R.isEmpty(oldVal)) {
+        setTimeout(() => {
+          this.searchbar = framework7.searchbar.create({
+            el: '#groups-searchbar',
+            searchContainer: '#groups-list',
+            searchIn: '.item-title'
+          })
+        }, 1000)
+      }
     }
   }
 }
