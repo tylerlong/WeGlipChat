045576fdbc69a2b3e96e9f38b638b63aaafe70f5
Tyler Long <tyler4long@gmail.com>
2018-08-01 13:51:23 +0800

    First working version of searching contacts

--- src/components/ContactList.vue
+++ src/components/ContactList.vue
@@ -1,8 +1,28 @@
 <template>
   <div class="page">
     <tabs active="contacts"></tabs>
+    <div class="navbar">
+      <div class="navbar-inner">
+        <div class="left"></div>
+        <div class="title">Contacts</div>
+        <div class="right"></div>
+        <div class="subnavbar">
+          <form class="searchbar">
+            <div class="searchbar-inner">
+              <div class="searchbar-input-wrap">
+                <input type="search" placeholder="Search">
+                <i class="searchbar-icon"></i>
+                <span class="input-clear-button"></span>
+              </div>
+              <span class="searchbar-disable-button">Cancel</span>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
     <div class="page-content">
-      <div class="list media-list" v-if="persons && persons.length > 0">
+      <div class="searchbar-backdrop"></div>
+      <div class="list media-list searchbar-found" v-if="persons && persons.length > 0">
         <ul>
           <li v-for="person in persons" :key="person.id" @click="openPerson(person.id)">
             <div class="item-content">
@@ -24,6 +44,9 @@
       <div v-else class="block text-align-center">
         <preloader></preloader>
       </div>
+      <div class="block searchbar-not-found">
+        <div class="block-inner">Nobody matched</div>
+      </div>
     </div>
   </div>
 </template>
@@ -33,6 +56,7 @@ import { mapGetters } from 'vuex'
 
 import Tabs from './Tabs.vue'
 import Preloader from './Preloader.vue'
+import framework7 from '../framework7'
 
 export default {
   components: {
@@ -48,6 +72,13 @@ export default {
     openPerson (id) {
       this.$router.push({ name: 'person', params: { id } })
     }
+  },
+  mounted: function () {
+    this.searchbar = framework7.searchbar.create({
+      el: '.searchbar',
+      searchContainer: '.list',
+      searchIn: '.item-title'
+    })
   }
 }
 </script>
